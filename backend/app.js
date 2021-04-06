d81a1b01acc1431f8978b84a998bf3cabb76a76d
rajatmaheshwari2512 <mrajat67@yahoo.com>
2021-04-06 21:00:03 +0530

    Finished App

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ var socketio = require("socket.io");
 
 var app = express();
 
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./socket/users");
+
 const server = require("http").createServer(app);
 
 const io = socketio(server, {
@@ -15,7 +22,7 @@ const io = socketio(server, {
     origin: "*",
   },
   pingTimeout: 1000,
-  pingInterval: 3000,
+  pingInterval: 1000,
 });
 
 app.use(logger("dev"));
@@ -32,6 +39,55 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500).json({ err: "Some Error Occured" });
+});
+
+io.on("connect", (socket) => {
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.join(user.room);
+    socket.emit("welcomeMessage", {
+      text: `Welcome to the Room, ${user.name}!`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit("welcomeMessage", { text: `${user.name} has joined.` });
+    console.log(`${user.name}:${room}`);
+    callback();
+  });
+
+  socket.on("sendTitle", (message, callback) => {
+    console.log(message);
+    const user = getUser(socket.id);
+    if (user) {
+      console.log(user);
+      io.to(user.room).emit("title", { text: message });
+    }
+    callback();
+  });
+
+  socket.on("sendBody", (input, callback) => {
+    console.log(input);
+    const user = getUser(socket.id);
+    if (user) {
+      console.log(user);
+      io.to(user.room).emit("body", { text: input });
+    }
+    callback();
+  });
+
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    console.log(user);
+    if (user) {
+      io.to(user.room).emit("disMess", {
+        text: `${user.name} has left the room`,
+      });
+    }
+  });
 });
 
 module.exports = { app: app, server: server };
--- frontend/src/components/Notes/Notes.js
+++ frontend/src/components/Notes/Notes.js
@@ -1,34 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import fetch from "cross-fetch";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import io from "socket.io-client";
 import queryString from "query-string";
-import { Input } from "antd";
+import { Input, message } from "antd";
+
 import "./Notes.css";
 
 const ENDPOINT = "http://localhost:3000";
 
 var socket;
+toast.configure();
 
 const { TextArea } = Input;
 
 export const Notes = ({ location }) => {
-  const [title, setTitle] = useState("");
+  const handleBody = (newBody) => {
+    setBody(newBody.target.value);
+    fetch(
+      `http://localhost:41184/notes/${room}?token=4f1ccc42bc33967c6d0100f4428105c4d0669df0e933e6facc84001dbb39a44856eddc8ecb5698921788045c28630a1d33f7c10787198c7b8e9e9c42b5d3dd69`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          body: newBody.target.value,
+        }),
+      }
+    ).then((res) => {
+      socket.emit("sendBody", newBody.target.value, () => {
+        console.log("New Body Sent");
+      });
+    });
+  };
+  const handleTitle = (newTitle) => {
+    setTitle(newTitle.target.value);
+    fetch(
+      `http://localhost:41184/notes/${room}?token=4f1ccc42bc33967c6d0100f4428105c4d0669df0e933e6facc84001dbb39a44856eddc8ecb5698921788045c28630a1d33f7c10787198c7b8e9e9c42b5d3dd69`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          title: newTitle.target.value,
+        }),
+      }
+    ).then((res) => {
+      socket.emit("sendTitle", newTitle.target.value, () => {
+        console.log("New Title Sent");
+      });
+    });
+  };
+
+  const [title, setTitle] = useState("Untitled");
   const [body, setBody] = useState("");
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
   const history = useHistory();
+
   useEffect(() => {
-    console.log(location.search);
     const { name, room } = queryString.parse(location.search);
-    setName(name);
-    setRoom(room);
+
     socket = io(ENDPOINT);
     socket.emit("join", { name, room }, (err) => {
       if (err) {
         history.push("/");
+        return toast.error(err);
       }
     });
+
+    setName(name);
+    setRoom(room);
+
     fetch(
       `http://localhost:41184/notes/${room}?token=4f1ccc42bc33967c6d0100f4428105c4d0669df0e933e6facc84001dbb39a44856eddc8ecb5698921788045c28630a1d33f7c10787198c7b8e9e9c42b5d3dd69&fields=title,body`,
       {
@@ -43,25 +84,39 @@ export const Notes = ({ location }) => {
         setBody(res.body);
       });
   }, [ENDPOINT, location.search]);
+
+  useEffect(() => {
+    socket.on("welcomeMessage", (message) => {
+      toast.success(message.text);
+    });
+    socket.on("title", (message) => {
+      setTitle(message.text);
+    });
+    socket.on("body", (message) => {
+      setBody(message.text);
+    });
+    socket.on("disMess", (message) => {
+      toast.warning(message.text);
+    });
+  }, []);
+
   return (
     <div className="notes-wrapper">
       {room && <h1> Room: {room}</h1>}
       <h1>Title:</h1>
-      {title && (
-        <Input
-          style={{ width: "300px", fontSize: "1.2rem" }}
-          placeholder="Note Title"
-          defaultValue={title}
-        />
-      )}
+      <TextArea
+        onChange={handleTitle}
+        style={{ width: "300px", fontSize: "1.2rem" }}
+        placeholder="Note Title"
+        value={title}
+      />
       <h1>Body:</h1>
-      {body && (
-        <TextArea
-          style={{ width: "70vw", height: "75vh", fontSize: "1.2rem" }}
-          rows={10}
-          defaultValue={body}
-        />
-      )}
+      <TextArea
+        onChange={handleBody}
+        style={{ width: "70vw", height: "75vh", fontSize: "1.2rem" }}
+        rows={10}
+        value={body}
+      />
     </div>
   );
 };
